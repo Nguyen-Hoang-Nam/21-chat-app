e214398d88e0e4bf5671d285541f4f8674efdec1
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-09-27 19:33:57 +0700

    ✨ Add Gateway and Eureka

---
+++ gateway/src/main/java/gateway/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package gateway.gateway;
+
+import com.github.mthizo247.cloud.netflix.zuul.web.socket.EnableZuulWebSocket;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@SpringBootApplication
+@EnableZuulWebSocket
+@EnableWebSocketMessageBroker
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
